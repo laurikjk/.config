1cf11a563268fe15ee9f05dd6baa801d6dde9844
laurikjk <kajakko.lauri@gmail.com>
2024-12-20 08:52:29 +0200

    nvim - change colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
+    "shaunsingh/nord.nvim",
+    name = "nord",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd("colorscheme kanagawa")
+        vim.cmd("colorscheme nord")
     end,
 }
 
