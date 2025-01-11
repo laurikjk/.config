68e5bf0e8e9e42a76239fb607953c6b8a5cf07c8
laurikjk <kajakko.lauri@gmail.com>
2025-01-11 13:55:22 +0200

    nvim - jupyter configs

---
+++ nvim/lua/plugins/jupyter.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    -- More info:
+    -- https://github.com/imbue-ai/jupyter_ascending.vim
+    -- https://medium.com/@ilia.ozhmegov/edit-jupyter-notebook-from-your-favorite-text-editor-dd48c5451d83
+    'untitled-ai/jupyter_ascending.vim'
+  }
+}
